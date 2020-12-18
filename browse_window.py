2aa8b708e658eb07f1a0c4848f6648fa1c9e8f0e
benrprince <55722294+benrprince@users.noreply.github.com>
2020-12-18 15:54:02 -0600

    Updated window buttons to work

--- browse_window.py
+++ browse_window.py
@@ -5,25 +5,60 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+import xlwt
+import patient_main as pm
+import pathlib
 
 
 class Window(Tk):
 
+    filename = ""
+
     def __init__(self):
         super(Window, self).__init__()
-        self.hello = ttk.Button(self)
+        self.run = ttk.Button(self)
+        self.browse = ttk.Button(self)
         self.title("Test Patient Generator")
-        self.minsize(640, 400)
+        self.minsize(430, 300)
         self.add_buttons()
 
     def add_buttons(self):
-        self.hello["text"] = "Hello World"
-        self.hello["command"] = self.say_hi
-        self.hello.pack(side="top")
+        self.run["text"] = "run"
+        self.run["command"] = self.run_program
+        self.run.pack(side="top")
+        self.browse["text"] = "Browse"
+        self.browse["command"] = self.browse_files
+        self.browse.pack(side="top")
 
     @staticmethod
-    def say_hi():
+    def run_program():
         print("Hello World")
+        global filename
+        try:
+            new_file = pm.patients(filename)
+            folder_select = filedialog.askdirectory()
+            num = ""
+            save_path = folder_select + '/patients' + num + '.xls'
+
+            while pathlib.Path(save_path).is_file():
+                if num == "":
+                    num = 0
+                num = str(int(num) + 1)
+                save_path = folder_select + '/patients' + num + '.xls'
+
+            new_file.save(save_path)
+
+        except NameError:
+            print("No File Selected")
+
+    @staticmethod
+    def browse_files():
+        global filename
+        filename = filedialog.askopenfilename(initialdir = "/",
+                                            title = "Select a File",
+                                            filetypes = (("Excel Files", "*.xlsx *.xls *.xlsm *.xlsb *.csv"), ("all files", "*.*")))
+        print(filename)
+
 
 
 window = Window()
