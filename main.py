ba1183d2929d493422fda02e7be2e823f09fcdfe
benrprince <55722294+benrprince@users.noreply.github.com>
2020-12-21 15:59:43 -0600

    Main file to run the project

---
+++ main.py
@@ -0,0 +1,12 @@
+# Author: Ben Prince
+# Version: 1.2
+# Description: Main file that is ran
+
+import browse_window as bw
+
+def main():
+    window = bw.Window()
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
